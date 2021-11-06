ac1ae4b363a549e20381bb4b6aff2165611c5497
Daniel Jung <danieljung335@gmail.com>
2021-11-06 19:35:18 -0400

    what is happening

--- src/components/a7/Build/index.js
+++ src/components/a7/Build/index.js
@@ -1,26 +1,18 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import ExploreScreen from "./ExploreScreen";
-import who from "./reducers/who";
-import tweets from "./reducers/tweets";
-import {combineReducers, createStore} from "redux";
-import {Provider} from "react-redux";
 import HomeScreen from "./HomeScreen";
 
-const reducers = combineReducers({tweets: tweets, who});
-const store = createStore(reducers);
 
 
 const Build = () => {
   return(
-      // <Provider store={store}>
       <div>
         <Route path={["/", "/a7/twitter/home"]}
                exact={true} component={HomeScreen}/>
         <Route path="/a7/twitter/explore"
                exact={true} component={ExploreScreen}/>
       </div>
-// </Provider>
   );
 };
 
