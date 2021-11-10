ae910cb3d69eb378c5debad075fd1e161f7a41d8
Daniel Jung <danieljung335@gmail.com>
2021-11-10 18:10:07 -0500

    surely this one must work

--- src/components/a8/Build/services/twitterService.js
+++ src/components/a8/Build/services/twitterService.js
@@ -1,4 +1,4 @@
-const TWEET_API = 'https://murmuring-sands-68812.herokuapp.com/';
+const TWEET_API = 'https://murmuring-sands-68812.herokuapp.com';
 
 export const fetchAllTweets = (dispatch) =>
     fetch(TWEET_API)
