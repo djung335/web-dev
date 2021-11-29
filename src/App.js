386540092af7b92f1f8ae87e521adf674af6304d
Daniel Jung <danieljung335@gmail.com>
2021-11-29 12:25:15 -0500

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Practice from "./components/a8/Practice";
-import Build from "./components/a9/Build";
 import {HashRouter, Link, Route} from "react-router-dom";
 import tweets from "./components/a8/Build/reducers/tweets";
 import who from "./components/a8/Build/reducers/who";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
-import ExploreScreen from "./components/a8/Build/ExploreScreen";
 import A9 from "./components/a9";
 
 const reducer = combineReducers({tweets: tweets, who});
